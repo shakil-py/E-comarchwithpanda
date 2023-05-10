e4fbaff004ac562d83e8bd26a130157af297da50
shakil-py <shakilhaquepy@gmail.com>
2023-05-10 12:12:19 +0600

    add all task js

--- mycode/js/panda.js
+++ mycode/js/panda.js
@@ -4,3 +4,23 @@ for (tag of h1tags) {
 }
 document.getElementById("bagpack").style.backgroundColor = "lightgreen"
 
+function clickhandel() {
+    console.log("Hi I am Shakil Haque")
+}
+const classesbtn = document.getElementsByClassName("btn")
+for (btn of classesbtn) {
+    btn.addEventListener("click", function () {
+        document.querySelector(".btn").parentNode.removeChild(document.querySelector(".btn"))
+    })
+}
+document.getElementById("form").addEventListener("dblclick", function () {
+    document.getElementById("form").style.backgroundColor = "red"
+}
+)
+document.getElementById("exampleInputEmail1").addEventListener("keyup", function(){
+    const text = document.getElementById("exampleInputEmail1").value
+    console.log(text)
+    if (text.includes("gmail")){
+        document.getElementById("mail-submit").removeAttribute("disabled")
+    }
+}) 
