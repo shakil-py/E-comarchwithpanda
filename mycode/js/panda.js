8036179e09cb52910b3dea6c2bfb86fd008f8138
shakil-py <shakilhaquepy@gmail.com>
2023-05-10 09:31:03 +0600

    add files

---
+++ mycode/js/panda.js
@@ -0,0 +1,10 @@
+const h1tags = document.getElementsByTagName("h2")
+console.log(h1tags)
+for (tag of h1tags) {
+    tag.style.color = "red"
+}
+document.getElementById("bagpack").style.backgroundColor = "lightgreen"
+const crdcalsses=document.getElementsByClassName("card")
+for(card of carcalsses){
+    card.style.borderrediouse="50px"
+}
--- myjs/panda.js
+++
@@ -1,5 +0,0 @@
-const h1tags=document.getElementsByTagName("h2")
-console.log(h1tags)
-for(tag of h1tags){
-    tag.style.color="red"
-}
